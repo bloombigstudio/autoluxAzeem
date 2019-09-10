dca4e79b8b5f143b1fda1ecc9f04afaa1cec5459
Azeem Arif <mazeemarif0@gmail.com>
2019-09-10 18:56:57 +0500

    search improved

--- auto/filters.py
+++ auto/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.db.models import Q
 from django.forms import TextInput, fields
 from auto.models import Product
 
@@ -25,10 +26,24 @@ class ProductFilter(django_filters.FilterSet):
         model = Product
         fields = ['product_title', 'product_category', 'product_price']
 
-    product_title = django_filters.CharFilter(lookup_expr='icontains')
+    # product_title = django_filters.CharFilter(lookup_expr='icontains')
+    product_title = django_filters.CharFilter(method='search_filter')
     sorting = django_filters.ChoiceFilter(label="Sort By", choices=SORT_CHOICES, method="filter_by_sort")
     product_category = django_filters.ChoiceFilter(label="Select category", choices=CATEGORY_CHOICES, lookup_expr='iexact')
     product_price = django_filters.RangeFilter()
+
+    def search_filter(self, queryset, name, value):
+        queryList = value.split()
+        queryList.append(value)
+        queries = [Q(product_title__icontains=value) for value in queryList]
+
+        query = queries.pop()
+
+        for item in queries:
+            query |= item
+
+        return Product.objects.filter(query)
+
 
     def filter_by_sort(self, queryset, name, value):
         if value == 'lowToHigh':
